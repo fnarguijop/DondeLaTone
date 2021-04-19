c7e6fb2f99fc32b97ac53d33b29250b8c0ef00de
JorgeAbrahan <jorge24abrahan@gmail.com>
2021-04-19 04:56:32 -0600

    Donde La Tone - Carrito css finished

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/* Selectors */
+const platesDescription = document.querySelectorAll('.plate-description');
+
+const platesName = document.querySelectorAll('.plate-name');
+
+const platesPrice = document.querySelectorAll('.price');
+
+const platesBuyButton = document.querySelectorAll('.buy-btn');
+
+platesDescription.forEach(function(plateDescription) {
+    plateDescription.addEventListener('mouseenter', () => {
+        plateDescription.style.transform = 'perspective(500px) translateZ(25px)';
+    });
+    plateDescription.addEventListener('mouseleave', () => {
+        plateDescription.style.transform = 'perspective(0) translateZ(0)';
+    });
+});
+
+platesName.forEach(function(plateName) {
+    plateName.addEventListener('mouseenter', () => {
+        plateName.style.transform = 'perspective(500px) translateZ(50px)';
+    });
+    plateName.addEventListener('mouseleave', () => {
+        plateName.style.transform = 'perspective(0) translateZ(0)';
+    });
+});
+
+platesPrice.forEach(function(platePrice) {
+    platePrice.addEventListener('mouseenter', () => {
+        platePrice.style.transform = 'perspective(500px) translateZ(50px)';
+    });
+    platePrice.addEventListener('mouseleave', () => {
+        platePrice.style.transform = 'perspective(0) translateZ(0)';
+    });
+});
+
+platesBuyButton.forEach(function(plateBuyButton) {
+    plateBuyButton.addEventListener('mouseenter', () => {
+        plateBuyButton.style.transform = 'perspective(500px) translateZ(50px)';
+    });
+    plateBuyButton.addEventListener('mouseleave', () => {
+        plateBuyButton.style.transform = 'perspective(0) translateZ(0)';
+    });
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,7 @@
-/* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("navbarMobile").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("navbarMobile").style.width = "0";
-  }
+  document.getElementById("navbarMobile").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navbarMobile").style.width = "0";
+}
